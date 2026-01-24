1692801718d9a70849c6dd1d2cc8da8249946b5f
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2026-01-24 23:04:40 +0545

    Create datanoteU14.js

---
+++ datanoteU14.js
@@ -0,0 +1,2 @@
+window.noteU14Content = `# Security Patterns Added (UNLocked)\n\n## Auth Table\n\n| Method | Level |\n|--------|-------|\n| JWT | High |\n| OAuth | Very High |`;
+window.noteU14Meta = { name: "Security ok", emoji: "🔒", password: null, protection:"YES"};
